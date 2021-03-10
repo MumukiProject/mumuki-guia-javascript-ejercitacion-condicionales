ea24198e8e88b0a2167dee21ac06420cce3e6833
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 16:21:24 +0000

    Mumuki Export on 2021-03-10 16:21:24 +0000

--- 00002_Está en rango/test..js
+++ 00002_Está en rango/test..js
@@ -1,5 +1,19 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos estaEnRango(3, 1, 10) debería retornar true", function() {
+  let resultado = estaEnRango(3, 1, 10)
+  assert.equal(resultado, true,`estaEnRango(3, 1, 10) deberia retornar true pero fue ${resultado}`);
+})
+
+it("Si hacemos estaEnRango(1, 1, 10) debería retornar true", function() {
+  let resultado = estaEnRango(1, 1, 10)
+  assert.equal(resultado, true,`estaEnRango(1, 1, 10) deberia retornar true pero fue ${resultado}`);
+})
+
+it("Si hacemos estaEnRango(12, 1, 10) debería retornar false", function() {
+  let resultado = estaEnRango(12, 1, 10)
+  assert.equal(resultado, false,`estaEnRango(12, 1, 10) deberia retornar false pero fue ${resultado}`);
+})
+
+it("Si hacemos estaEnRango(-3, 1, 10) debería retornar false", function() {
+  let resultado = estaEnRango(-3, 1, 10)
+  assert.equal(resultado, false,`estaEnRango(-3, 1, 10) deberia retornar false pero fue ${resultado}`);
 })
