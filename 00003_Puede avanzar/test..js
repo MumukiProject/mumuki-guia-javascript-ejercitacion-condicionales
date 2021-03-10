e4ce0bf8eb07e69ed7af1b93d1ec714c74ec0f72
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 16:34:55 +0000

    Mumuki Export on 2021-03-10 16:34:55 +0000

--- 00003_Puede avanzar/test..js
+++ 00003_Puede avanzar/test..js
@@ -1,5 +1,19 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos puedeAvanzar('verde') debería retornar true", function() {
+  let resultado = puedeAvanzar('verde')
+  assert.equal(resultado, true,`puedeAvanzar('verde') deberia retornar true pero fue ${resultado}`);
+})
+
+it("Si hacemos puedeAvanzar('amarillo') debería retornar false", function() {
+  let resultado = puedeAvanzar('amarillo')
+  assert.equal(resultado, false,`puedeAvanzar('amarillo') deberia retornar false pero fue ${resultado}`);
+})
+
+it("Si hacemos puedeAvanzar('rojo') debería retornar false", function() {
+  let resultado = puedeAvanzar('rojo')
+  assert.equal(resultado, false,`puedeAvanzar('rojo') deberia retornar false pero fue ${resultado}`);
+})
+
+it("Si hacemos puedeAvanzar('naranja') debería retornar Error: color de semáforo inválido", function() {
+  let resultado = puedeAvanzar('naranja')
+  assert.equal(resultado, 'Error: color de semáforo inválido',`puedeAvanzar('naranja') deberia retornar Error: color de semáforo inválido pero fue ${resultado}`);
 })
