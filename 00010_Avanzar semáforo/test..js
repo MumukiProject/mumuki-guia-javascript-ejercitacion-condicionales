07e6fc02edd41cd08d990355a7d4f16ebb20d6f9
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 18:12:19 +0000

    Mumuki Export on 2021-03-10 18:12:19 +0000

--- 00010_Avanzar semáforo/test..js
+++ 00010_Avanzar semáforo/test..js
@@ -1,4 +1,14 @@
-it("Si hacemos esPositivoONegativo(-5) debería retornar 'negativo'", function() {
-let resultado = esPositivoONegativo(-5)
-assert.equal(resultado, 'negativo',`esPositivoONegativo(-5) deberia retornar 'negativo' pero fue ${resultado}`);
+it("Si hacemos avanzarSemaforo('verde') debería retornar 'amarillo'", function() {
+let resultado = avanzarSemaforo('verde')
+assert.equal(resultado, 'amarillo',`avanzarSemaforo('verde') deberia retornar 'amarillo' pero fue ${resultado}`);
+})
+
+it("Si hacemos avanzarSemaforo('amarillo') debería retornar 'rojo'", function() {
+let resultado = avanzarSemaforo('amarillo')
+assert.equal(resultado, 'rojo',`avanzarSemaforo('amarillo') deberia retornar 'rojo' pero fue ${resultado}`);
+})
+
+it("Si hacemos avanzarSemaforo('rojo') debería retornar 'verde'", function() {
+let resultado = avanzarSemaforo('rojo')
+assert.equal(resultado, 'verde',`avanzarSemaforo('rojo') deberia retornar 'verde' pero fue ${resultado}`);
 })
