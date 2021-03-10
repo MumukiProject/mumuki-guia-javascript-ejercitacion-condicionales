5896710ac42f763e7564c934108d844915bcd896
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 18:57:32 +0000

    Mumuki Export on 2021-03-10 18:57:32 +0000

--- 00011_Obtener sensación/description.md
+++ 00011_Obtener sensación/description.md
@@ -1,5 +1,5 @@
-Crear una función `obtenerSensacion` que tome como argumento un número `temperatura` y devuelva un string dependiendo de la `temperatura`, con las siguientes reglas:
-
+> Definí una función `obtenerSensacion` que tome como argumento un número `temperatura` y devuelva un string dependiendo de la `temperatura`, con las siguientes reglas:
+>
 | Temperatura | Mensaje |
 | --- | --- |
 | Menor a 0° | ¡Está helando!
@@ -7,7 +7,8 @@ Crear una función `obtenerSensacion` que tome como argumento un número `temper
 | Mayor o igual a 15° y menor a 25° | Está lindo
 | Mayor o igual a entre 25° y menor a 30° | Hace calor
 | Mayor o igual de 30° | ¡Hace mucho calor!
-
+>
 ```javascript
-console.log(obtenerSensacion(33)) // "¡Hace mucho calor!"
+ム obtenerSensacion(33)
+"¡Hace mucho calor!"
 ```
--- 00011_Obtener sensación/test..js
+++ 00011_Obtener sensación/test..js
@@ -1,5 +1,19 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos obtenerSensacion(33) debería retornar ¡Hace mucho calor!", function() {
+let resultado = obtenerSensacion(33)
+assert.equal(resultado, '¡Hace mucho calor!',`obtenerSensacion(33) deberia retornar '¡Hace mucho calor!' pero fue ${resultado}`);
+})
+
+it("Si hacemos obtenerSensacion(-3) debería retornar ¡Está helando!", function() {
+let resultado = obtenerSensacion(-3)
+assert.equal(resultado, '¡Está helado!',`obtenerSensacion(-3) deberia retornar '¡Está helado!' pero fue ${resultado}`);
+})
+
+it("Si hacemos obtenerSensacion(15) debería retornar Está Lindo", function() {
+let resultado = obtenerSensacion(15)
+assert.equal(resultado, 'Está lindo',`obtenerSensacion(15) deberia retornar 'Está lindo' pero fue ${resultado}`);
+})
+
+it("Si hacemos obtenerSensacion(27) debería retornar Hace calor", function() {
+let resultado = obtenerSensacion(27)
+assert.equal(resultado, 'Hace calor',`obtenerSensacion(27) deberia retornar 'Hace calor' pero fue ${resultado}`);
 })
