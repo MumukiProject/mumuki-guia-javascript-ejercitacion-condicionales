498c09dcf311857d4ffbbf0b317d11351300c2f1
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 18:05:32 +0000

    Mumuki Export on 2021-03-10 18:05:32 +0000

--- 00009_Positivo o negativo/test..js
+++ 00009_Positivo o negativo/test..js
@@ -5,6 +5,10 @@ assert.equal(resultado, 'positivo',`esPositivoONegativo(3) deberia retornar 'pos
 
 it("Si hacemos esPositivoONegativo(-5) debería retornar 'negativo'", function() {
 let resultado = esPositivoONegativo(-5)
-assert.equal(resultado, 'negativo',`esPositivoONegativo(-5) deberia retornar 'positinegativovo' pero fue ${resultado}`);
+assert.equal(resultado, 'negativo',`esPositivoONegativo(-5) deberia retornar 'negativo' pero fue ${resultado}`);
 })
 
+it("Si hacemos esPositivoONegativo(15) debería retornar 'positivo'", function() {
+let resultado = esPositivoONegativo(15)
+assert.equal(resultado, 'positivo',`esPositivoONegativo(15) deberia retornar 'positivo' pero fue ${resultado}`);
+})
