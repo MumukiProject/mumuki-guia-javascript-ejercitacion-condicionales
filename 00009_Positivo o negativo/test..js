02ee54f5b65e23cb2e719e2df0d214802cf27444
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 18:02:40 +0000

    Mumuki Export on 2021-03-10 18:02:40 +0000

--- 00009_Positivo o negativo/test..js
+++ 00009_Positivo o negativo/test..js
@@ -1,5 +1,10 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("Si hacemos esPositivoONegativo(3) debería retornar 'positivo'", function() {
+let resultado = esPositivoONegativo(3)
+assert.equal(resultado, 'positivo',`esPositivoONegativo(3) deberia retornar 'positivo' pero fue ${resultado}`);
 })
+
+it("Si hacemos esPositivoONegativo(-5) debería retornar 'negativo'", function() {
+let resultado = esPositivoONegativo(-5)
+assert.equal(resultado, 'negativo',`esPositivoONegativo(-5) deberia retornar 'positinegativovo' pero fue ${resultado}`);
+})
+
