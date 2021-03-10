20e9e23172296289fd499fda477f56b1e6ac573b
mayra@mumuki.org <mayra@mumuki.org>
2021-03-10 17:30:38 +0000

    Mumuki Export on 2021-03-10 17:30:38 +0000

--- 00003_Puede avanzar/test..js
+++ 00003_Puede avanzar/test..js
@@ -1,19 +1,19 @@
 it("Si hacemos puedeAvanzar('verde') debería retornar true", function() {
-  let resultado = puedeAvanzar('verde')
-  assert.equal(resultado, true,`puedeAvanzar('verde') deberia retornar true pero fue ${resultado}`);
+let resultado = puedeAvanzar('verde')
+assert.equal(resultado, true,`puedeAvanzar('verde') deberia retornar true pero fue ${resultado}`);
 })
 
 it("Si hacemos puedeAvanzar('amarillo') debería retornar false", function() {
-  let resultado = puedeAvanzar('amarillo')
-  assert.equal(resultado, false,`puedeAvanzar('amarillo') deberia retornar false pero fue ${resultado}`);
+let resultado = puedeAvanzar('amarillo')
+assert.equal(resultado, false,`puedeAvanzar('amarillo') deberia retornar false pero fue ${resultado}`);
 })
 
 it("Si hacemos puedeAvanzar('rojo') debería retornar false", function() {
-  let resultado = puedeAvanzar('rojo')
-  assert.equal(resultado, false,`puedeAvanzar('rojo') deberia retornar false pero fue ${resultado}`);
+let resultado = puedeAvanzar('rojo')
+assert.equal(resultado, false,`puedeAvanzar('rojo') deberia retornar false pero fue ${resultado}`);
 })
 
 it("Si hacemos puedeAvanzar('naranja') debería retornar Error: color de semáforo inválido", function() {
-  let resultado = puedeAvanzar('naranja')
-  assert.equal(resultado, 'Error: color de semáforo inválido',`puedeAvanzar('naranja') deberia retornar Error: color de semáforo inválido pero fue ${resultado}`);
+let resultado = puedeAvanzar('naranja')
+assert.equal(resultado, 'Error: color de semáforo inválido',`puedeAvanzar('naranja') deberia retornar Error: color de semáforo inválido pero fue ${resultado}`);
 })
