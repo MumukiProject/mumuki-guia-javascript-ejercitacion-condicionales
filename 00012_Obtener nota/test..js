839bf1a03540c990f7a1a4e7797ba4c9c304f84a
mayra@mumuki.org <mayra@mumuki.org>
2021-03-15 12:33:50 +0000

    Mumuki Export on 2021-03-15 12:33:50 +0000

--- 00012_Obtener nota/test..js
+++ 00012_Obtener nota/test..js
@@ -8,7 +8,7 @@ let resultado = obtenerNota(9.6)
 assert.equal(resultado, 'Muy bueno',`obtenerNota(9.6) deberia retornar 'Muy bueno' pero fue ${resultado}`);
 })
 
-it("Si hacemos obtenerNota(12) debería retornar Puntaje invalido", function() {
+it("Si hacemos obtenerNota(12) debería retornar Puntaje inválido", function() {
 let resultado = obtenerNota(12)
-assert.equal(resultado, 'Puntaje invalido',`obtenerNota(12) deberia retornar 'Puntaje inválido' pero fue ${resultado}`);
+assert.equal(resultado, 'Puntaje inválido',`obtenerNota(12) deberia retornar 'Puntaje inválido' pero fue ${resultado}`);
 })
